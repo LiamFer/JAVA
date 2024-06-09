7b34def738db633e92ecce9790c64a01b728a896
William Fernandes <wiilfern1910@outlook.com>
2024-06-09 11:04:21 -0300

    Variables

--- main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        String test = "1";
-        System.out.println("Hello World {0}");
-    }
-}
---
+++ mainProgram.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class mainProgram {
+    public static void main(String[] args) {
+        // Comment
+        double myNum = 10.21;
+        String myName = "Gauss";
+        float avg = 8.25f;
+
+        
+        System.out.print("One line escape sequence testing\n");
+        System.out.println("Hello World " + myNum);
+
+    }
+}
