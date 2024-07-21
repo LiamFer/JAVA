30699f2e382746d2e7037d5b78e7e499f3e2546b
William Fernandes <wiilfern1910@outlook.com>
2024-07-21 09:11:52 -0300

    Creating a Class

---
+++ Intro/Car.java
@@ -0,0 +1,15 @@
+package Intro;
+
+public class Car {
+    String model = "Muscle Car";
+    String color = "Red";
+    int year = 1990;
+    double horsepower = 1.5;
+
+    void drive(){
+        System.out.println("You started driving the car");
+    }
+    void brake(){
+        System.out.println("You stopped brakinging the car");
+    }
+}
---
+++ Intro/OOP.java
@@ -0,0 +1,9 @@
+package Intro;
+
+public class OOP {
+    public static void main(String[] args) {
+        Car mycar = new Car();
+        System.out.println(mycar.model);
+        System.out.println(mycar.color);
+    }
+}
